0351ebf22985e9c7352dc4db69cd0e77d298d96a
unknown <jinyeob.kim.0818@gmail.com>
2018-02-12 10:48:33 -0600

    some db stuff

--- 10q/server/index.js
+++ 10q/server/index.js
@@ -1,40 +1,43 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
+
+const { PORT, CONNECTION_STRING, SECRET } = process.env;
 
 //  MIDDLEWARE DEP
-const { json } = require('body-parser');
-const session = require('express-session');
-const cors = require('cors');
+const { json } = require("body-parser");
+const session = require("express-session");
+const cors = require("cors");
 
 // DATABASE DEP
-const massive = require('massive');
-const mainCtrl = require('./mainCtrl');
+const massive = require("massive");
+// const mainCtrl = require("./mainCtrl");
 
 //INITIALIZE APP
 const app = express();
 
-
+//SOCKET.IO
+// const socket = require("socket.io");
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+let socketCount = 0;
 // CONSTS FOR FUNCTIONS
-const{  }= require('./mainCtrl')
-
-const port = process.env.SERVER_PORT || 3001;
-const connectionString = process.env.CONNECTION_STRING;
+// const {} = require("./mainCtrl");
 
 //MASSIVE CONNECTION TO DB
-massive(process.env.CONNECTION_STRING)
-.then(db => {
-   app.set( 'db', db )
-})
-.catch(console.log);
-console.log('HIT');
+massive(CONNECTION_STRING)
+  .then(db => {
+    app.set("db", db);
+  })
+  .catch(console.log);
+// console.log("HIT");
 
-//BASIC MIDDLEWARES 
+//BASIC MIDDLEWARES
 app.use(json());
 app.use(cors());
 
 // app.use(
 //    session({
-//        secret: process.env.SECRET,
+//        secret: SECRET,
 //        saveUninitialized: false,
 //        resave: false,
 //        cookie: {
@@ -43,15 +46,21 @@ app.use(cors());
 //    })
 // );
 
-app.get('/api/test', (req,res,next) => {
-   app
-       .get('db')
-       .person.find({})
-       .then(response => {
-           res.json(response);
-       });
+io.on("connection", socket => {
+  socketCount++;
+  console.log("Client connected: ", socket);
+  socket.on("disconnect", () => console.log("Client disconnected"));
 });
 
-app.listen(port, () => {
-   console.log(`Listening on port: ${port}`);
+app.get("/api/test", (req, res, next) => {
+  app
+    .get("db")
+    .person.find({})
+    .then(response => {
+      res.json(response);
+    });
+});
+
+http.listen(PORT || 3001, () => {
+  console.log(`Listening on port: ${PORT}`);
 });
