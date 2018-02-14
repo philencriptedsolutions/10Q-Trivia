a83f63615962af1386750911767a1727a12f00ee
unknown <jinyeob.kim.0818@gmail.com>
2018-02-14 12:32:34 -0600

    conflict

--- 10q/src/Components/Quiz/Quiz.js
+++ 10q/src/Components/Quiz/Quiz.js
@@ -9,44 +9,37 @@ import Completed from '../SubComponents/Completed/Completed';
 import Header from '../SubComponents/Header/Header';
 
 class Quiz extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      response:{},
+      questionNumber:0
+      in:true;
+    }
+  }
 
   componentDidMount(){
     this.socket = openSocket();
-    this.socket.on("new question", question => this.props.saveNewQuestion( question ));
-    this.socket.on("new answer", newinfo =>  this.props.changeToAnswerView() );
-    
+    this.socket.on("new question", question => this.setState({ response : question }));
+    this.socket.on("update question number", questionNumber => {
+      this.setState({ questionNumber });
+    });
+
   }
 
   submitAnswer(answerSelected){
-    
-    // Here we can check to see if their selected answer is the same as the right asnwer.
-    // It should also only fire off after the time out.  
-    const { canContinue } = this.state.props;
-    this.socket.emit("answer selected", canContinue);
+    this.socket.emit("answer selected", in => this.setState({ in }));
   }
 
   render(){
-    console.log("props",this.props);
-    const { isQuestion, isAnswer, endOfGame } = this.props.quizReducer;
-    let whatShows, host;
+    const { question, answer, completedNum } = this.state.response;
+    let whatShows;
 
-    if( host ){
-      host = (
-        <Host>
-          "This is where the Live Streaming is gonna happen"
-        </Host>
-      );
-    } else {
-      host = (<Host>
-          {`The Game Starts in 4 seconds`}
-        </Host>);
-    }
-
-    if( isQuestion && !( endOfGame ) ){
+    if( question && !(answer) && !( completedNum ) {
       whatShows = < Question questionObject={ this.state.response }/>;
-    } else if(  isAnswer && !( endOfGame ) ) {
+    } else if( !( question ) && answer && !( completedNum ) ) {
       whatShows = < Answer answerObject={ this.state.response }/>;
-    } else if( endOfGame ) {
+    } else if( completedNum ) {
       whatShows = < Completed completedObject={ this.state.reponse }/>;
     } else {
       whatShows = null;
@@ -55,8 +48,7 @@ class Quiz extends Component {
     return (
       <div className="Quiz">
         <Header/>
-       { host }
-       { this.props.loginReducer.user.uid === 1 ? ( <div><button onClick={ () => this.handleGameStart }></button>Make Game Button Clickable</div> ) : null }
+       { this.state.admin ? ( <div><button onClick={ () => this.handleGameStart }></button>Make Game Button Clickable</div> ) : null }
        { whatShows }
       </div>
     )
