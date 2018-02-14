7d96799aa11eafc2c0e9b7108f1fc188d588f806
unknown <jinyeob.kim.0818@gmail.com>
2018-02-14 15:25:34 -0600

    Question page styling and Header

--- 10q/src/Components/Quiz/Quiz.js
+++ 10q/src/Components/Quiz/Quiz.js
@@ -1,57 +1,64 @@
-import React , { Component } from 'react';
+import React, { Component } from "react";
 import openSocket from "socket.io-client";
 //import './Quiz.css';
 
-import Question from '../SubComponents/Question/Question';
-import Host from '../SubComponents/Host/Host';
-import Answer from '../SubComponents/Answer/Answer';
-import Completed from '../SubComponents/Completed/Completed';
-import Header from '../SubComponents/Header/Header';
+import Question from "../SubComponents/Question/Question";
+import Host from "../SubComponents/Host/Host";
+import Answer from "../SubComponents/Answer/Answer";
+import Completed from "../SubComponents/Completed/Completed";
+import Header from "../SubComponents/Header/Header";
 
 class Quiz extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      response:{},
-      questionNumber:0,
-      in:true
-    }
+      response: {},
+      questionNumber: 0,
+      in: true
+    };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.socket = openSocket();
-    this.socket.on("new question", question => this.setState({ response : question }));
+    this.socket.on("new question", question =>
+      this.setState({ response: question })
+    );
     this.socket.on("update question number", questionNumber => {
       this.setState({ questionNumber });
     });
-
   }
 
-  submitAnswer(answerSelected){
-    this.socket.emit("answer selected", in => this.setState({ in }));
-  }
+  // submitAnswer(answerSelected){
+  //   this.socket.emit("answer selected", in => this.setState({ in }));
+  // }
 
-  render(){
-    const { question, answer, completedNum } = this.state.response;
-    let whatShows;
+  render() {
+    // const { question, answer, completedNum } = this.state.response;
+    // let whatShows;
 
-    if( question && !(answer) && !( completedNum ) {
-      whatShows = < Question questionObject={ this.state.response }/>;
-    } else if( !( question ) && answer && !( completedNum ) ) {
-      whatShows = < Answer answerObject={ this.state.response }/>;
-    } else if( completedNum ) {
-      whatShows = < Completed completedObject={ this.state.reponse }/>;
-    } else {
-      whatShows = null;
-    }
+    // if( question && !(answer) && !( completedNum ) {
+    //   whatShows = < Question questionObject={ this.state.response }/>;
+    // } else if( !( question ) && answer && !( completedNum ) ) {
+    //   whatShows = < Answer answerObject={ this.state.response }/>;
+    // } else if( completedNum ) {
+    //   whatShows = < Completed completedObject={ this.state.reponse }/>;
+    // } else {
+    //   whatShows = null;
+    // }
 
     return (
       <div className="Quiz">
-        <Header/>
-       { this.state.admin ? ( <div><button onClick={ () => this.handleGameStart }></button>Make Game Button Clickable</div> ) : null }
-       { whatShows }
+        <Header />
+        {/* {this.state.admin ? (
+          <div>
+            <button onClick={() => this.handleGameStart} />Make Game Button
+            Clickable
+          </div>
+        ) : null}
+        {whatShows} */}
+        <Question />
       </div>
-    )
+    );
   }
 }
 export default Quiz;
--- 10q/src/Components/SubComponents/Header/Header.js
+++ 10q/src/Components/SubComponents/Header/Header.js
@@ -1,77 +1,82 @@
-import React, { Component } from 'react';
-import AppBar from 'material-ui/AppBar';
-import { fire as firebase } from '../../../../src/fire';
-import { NavLink } from 'react-router-dom'
+import React, { Component } from "react";
+import AppBar from "material-ui/AppBar";
+import { fire as firebase } from "../../../../src/fire";
+import { NavLink } from "react-router-dom";
 import HardwareVideogameAsset from "material-ui/svg-icons/hardware/videogame-asset";
 import ActionAccountCircle from "material-ui/svg-icons/action/account-circle";
-import ActionExitToApp from 'material-ui/svg-icons/action/exit-to-app';
+import ActionExitToApp from "material-ui/svg-icons/action/exit-to-app";
 import Drawer from "material-ui/Drawer";
 import MenuItem from "material-ui/MenuItem";
 import NavigationMenu from "material-ui/svg-icons/navigation/menu";
-
+import "./Header.css";
 
 class Header extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            opened: false,
+  constructor(props) {
+    super(props);
+    this.state = {
+      opened: false
+    };
 
+    this.signOut = this.signOut.bind(this);
+    this.toggleDrawer = this.toggleDrawer.bind(this);
+  }
+
+  signOut() {
+    firebase
+      .auth()
+      .signOut()
+      .then(
+        () => {
+          window.location.href = "http://localhost:3000/";
+        },
+        function(error) {
+          console.error("Sign Out Error", error);
         }
+      );
+  }
 
-        this.signOut = this.signOut.bind(this);
-        this.toggleDrawer = this.toggleDrawer.bind(this);
+  toggleDrawer() {
+    const { opened } = this.state;
+    this.setState({
+      opened: !opened
+    });
+  }
 
-    }
+  render() {
+    return (
+      <div className="header-main">
+        <AppBar
+          //iconElementRight={`Logo`}
+          className="header-bar"
+          iconElementLeft={<NavigationMenu />}
+          iconClassNameLeft="header-burger"
+          iconClassNameRight="header-logo"
+          onLeftIconButtonClick={() => this.toggleDrawer()}
+        />
 
-    signOut() {
-        firebase.auth().signOut().then(() => {
-            window.location.href = "http://localhost:3000/"
-        }, function (error) {
-            console.error('Sign Out Error', error);
-        });
-    }
-
-    toggleDrawer() {
-        const { opened } = this.state;
-        this.setState({
-            opened: !opened
-        });
-    }
-
-
-    render() {
-        return (
-            <div>
-                <AppBar
-                    //iconElementRight={`Logo`}
-                    iconElementLeft={
-                        <NavigationMenu />
-                    }
-                    onLeftIconButtonClick={() => this.toggleDrawer()}
-                />
-
-                <Drawer
-                    open={this.state.opened}
-                    docked={false}
-                    onRequestChange={() => this.toggleDrawer()}
-                >
-                    <MenuItem leftIcon={<HardwareVideogameAsset />}>
-                        <NavLink to="/Quiz">
-                            <p > Game </p>
-                        </NavLink>
-                    </MenuItem>
-                    <MenuItem leftIcon={<ActionAccountCircle />}>
-                        <NavLink to="/profile">
-                            <p > Profile </p>
-                        </NavLink>
-                    </MenuItem>
-                    <MenuItem leftIcon={<ActionExitToApp />}>
-                        <p onClick={this.signOut} > Logout </p>
-                    </MenuItem>
-                </Drawer>
-            </div>
-
-        )
-    }
+        <Drawer
+          anchorOrigin="top"
+          open={this.state.opened}
+          docked={false}
+          width={60}
+          onRequestChange={() => this.toggleDrawer()}
+        >
+          <MenuItem>
+            <NavLink to="/Quiz">
+              <HardwareVideogameAsset style={{ margin: "20px 0 0 0" }} />
+            </NavLink>
+          </MenuItem>
+          <MenuItem>
+            <NavLink to="/profile">
+              <ActionAccountCircle />
+            </NavLink>
+          </MenuItem>
+          <MenuItem>
+            <ActionExitToApp onClick={this.signOut} />
+          </MenuItem>
+        </Drawer>
+      </div>
+    );
+  }
 }
-export default Header
+export default Header;
