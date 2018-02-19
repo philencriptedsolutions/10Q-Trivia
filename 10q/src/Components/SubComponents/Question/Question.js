93174d93658ec2e6bff7625c6a4b0315236debad
unknown <jinyeob.kim.0818@gmail.com>
2018-02-19 13:58:33 -0600

    redirect when not logged in, chat wrap, timer

--- 10q/src/Components/SubComponents/Question/Question.js
+++ 10q/src/Components/SubComponents/Question/Question.js
@@ -1,11 +1,40 @@
 import Avatar from "material-ui/Avatar";
 import SocialPeople from "material-ui/svg-icons/social/people";
+import CircularProgress from "material-ui/CircularProgress";
+
 import { connect } from "react-redux";
 import { changeToWrong, handleAnswer } from "../../../ducks/quizReducer";
 import React, { Component } from "react";
 import "./Question.css";
 
 class Question extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentCount: 100,
+      intervalId: 0
+    };
+    this.timer = this.timer.bind(this);
+  }
+
+  componentDidMount() {
+    let intervalId = setInterval(this.timer, 1000);
+    this.setState({
+      intervalId: intervalId
+    });
+  }
+
+  timer() {
+    this.setState({
+      currentCount: this.state.currentCount - 10
+    });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.intervalId);
+  }
+
   render() {
     const {
       question = [],
@@ -18,69 +47,76 @@ class Question extends Component {
     return (
       <div className="question-main">
         <div className="question-card">
-        
-        
-        
           <div className="players-list">
-            <SocialPeople className="people-icon"/>
+            <SocialPeople className="people-icon" />
             <i className="player-list">{playerList}</i>
           </div>
-          <Avatar
-            src="https://pickaface.net/gallery/avatar/totage5611dac58af1e.png"
-            size={62.5}
-            className="host-avatar"
-          />
+          <div className="progress-wrapper">
+            <Avatar
+              src="https://pickaface.net/gallery/avatar/totage5611dac58af1e.png"
+              size={60}
+              className="host-avatar"
+            />
+            <CircularProgress
+              mode="determinate"
+              value={this.state.currentCount}
+              size={75}
+              style={{ position: "relative", fill: "none" }}
+              thickness={6}
+              color="#0e89f3"
+            />
+          </div>
+          {/* {this.state.currentCount} */}
 
-        
-          <p className="question-text">{question[0].question}</p>
+          <p className="question-text">{question.question}</p>
           <div className="choices-container">
             <button
               disabled={userChoice || wrong}
               className="question-button"
               style={
-                userChoice === question[0].first_answer
+                userChoice === question.first_answer
                   ? {
                       backgroundColor: "#0e89f3",
                       color: "#ffffff"
                     }
                   : {}
               }
-              value={question[0].first_answer}
+              value={question.first_answer}
               onClick={e => handleAnswer(e.target.value)}
             >
-              {question[0].first_answer}
+              {question.first_answer}
             </button>
             <button
               disabled={userChoice || wrong}
               className="question-button"
               style={
-                userChoice === question[0].second_answer
+                userChoice === question.second_answer
                   ? {
                       backgroundColor: "#0e89f3",
                       color: "#ffffff"
                     }
                   : {}
               }
-              value={question[0].second_answer}
+              value={question.second_answer}
               onClick={e => handleAnswer(e.target.value)}
             >
-              {question[0].second_answer}
+              {question.second_answer}
             </button>
             <button
               disabled={userChoice || wrong}
               className="question-button"
               style={
-                userChoice === question[0].third_answer
+                userChoice === question.third_answer
                   ? {
                       backgroundColor: "#0e89f3",
                       color: "#ffffff"
                     }
                   : {}
               }
-              value={question[0].third_answer}
+              value={question.third_answer}
               onClick={e => handleAnswer(e.target.value)}
             >
-              {question[0].third_answer}
+              {question.third_answer}
             </button>
           </div>
         </div>
