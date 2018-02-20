08f487392af7f706654d97f36289cba914a8a0c3
unknown <jinyeob.kim.0818@gmail.com>
2018-02-20 14:52:46 -0600

    emoticon

--- 10q/src/Components/SubComponents/Chat/Chat.js
+++ 10q/src/Components/SubComponents/Chat/Chat.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import ContentSend from "material-ui/svg-icons/content/send";
+import ImageTagFaces from "material-ui/svg-icons/image/tag-faces";
+import NavigationClose from "material-ui/svg-icons/navigation/close";
 import TextField from "material-ui/TextField";
+import Dialog from "material-ui/Dialog";
+import EmojiPicker from "emoji-picker-react";
+import Emojify from "react-emojione";
 import { connect } from "react-redux";
 import "./Chat.css";
 
@@ -10,13 +15,17 @@ class Chat extends Component {
 
     this.state = {
       message: "",
-      messageList: []
+      emoji: "",
+      messageList: [],
+      open: false
     };
 
     this.userTyping = this.userTyping.bind(this);
     this.handleChat = this.handleChat.bind(this);
     this.addMessage = this.addMessage.bind(this);
     this.confirmTyping = this.confirmTyping.bind(this);
+    this.handleEmoji = this.handleEmoji.bind(this);
+    this.handleModal = this.handleModal.bind(this);
   }
 
   componentDidMount() {
@@ -38,6 +47,12 @@ class Chat extends Component {
   userTyping(message) {
     this.setState({
       message
+    });
+  }
+
+  handleEmoji(emojiName, emojiData) {
+    this.setState({
+      message: this.state.message.concat(`:${emojiData.name}:`)
     });
   }
 
@@ -66,6 +81,12 @@ class Chat extends Component {
     }
   }
 
+  handleModal() {
+    this.setState({
+      open: !this.state.open
+    });
+  }
+
   render() {
     this.props.socket.emit("works");
     console.log(this.state.messageList);
@@ -76,8 +97,10 @@ class Chat extends Component {
             <img className="chat-img" src={message.img} alt="" />
           </div>
           <div className="chat-content">
-            <span>{message.user} </span>
-            <p>{message.message}</p>
+            <Emojify style={{ height: 18, width: 18, margin: "3px 0 0 0" }}>
+              <span>{message.user} </span>
+              <p>{message.message}</p>
+            </Emojify>
           </div>
         </div>
       );
@@ -102,6 +125,22 @@ class Chat extends Component {
             value={this.state.message}
             id="text-field"
           />
+          <ImageTagFaces onClick={this.handleModal} className="chat-send" />
+          <Dialog
+            bodyClassName="modal-body"
+            actionsContainerClassName="modal-action"
+            contentClassName="modal-content"
+            paperClassName="modal-paper"
+            open={this.state.open}
+            actions={
+              <NavigationClose
+                onClick={this.handleModal}
+                className="chat-send"
+              />
+            }
+          >
+            <EmojiPicker onEmojiClick={this.handleEmoji} />
+          </Dialog>
           <ContentSend onClick={this.handleChat} className="chat-send" />
         </div>
       </div>
