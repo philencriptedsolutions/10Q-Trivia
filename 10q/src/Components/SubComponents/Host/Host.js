17674696d814493050b22db05e6990fede5d75fd
Josh Godinez <joshua.e.godinez@gmail.com>
2018-03-01 16:06:38 -0600

    added videos

--- 10q/src/Components/SubComponents/Host/Host.js
+++ 10q/src/Components/SubComponents/Host/Host.js
@@ -8,8 +8,17 @@ class Host extends Component {
 
     this.state = {
       videos : [ 
-        "https://firebasestorage.googleapis.com/v0/b/qtrivia-4dbfa.appspot.com/o/The%20Hunger%20Games%20Katniss%20and%20Peeta%20Reaping%20Scene%20%5BHD%5D%20(2).mp4?alt=media&token=a62be25e-d4da-47b9-880d-629b607080aa", 
-        "https://firebasestorage.googleapis.com/v0/b/snapdesign-1c0bb.appspot.com/o/landingvideo.mp4?alt=media&token=8f0bd940-9b77-4e5c-8dc2-658fb419a34a"
+        "https://firebasestorage.googleapis.com/v0/b/qtrivia-4dbfa.appspot.com/o/newIntroduction.mp4?alt=media&token=69d56f3f-0ba4-45fc-b57e-5a545dce0350", 
+        "https://firebasestorage.googleapis.com/v0/b/qtrivia-4dbfa.appspot.com/o/lindseyax.mp4?alt=media&token=ca496fea-6386-44e2-ac13-32ce887f067e",
+        "https://firebasestorage.googleapis.com/v0/b/qtrivia-4dbfa.appspot.com/o/Trees.mp4?alt=media&token=d647ae5d-0901-4b2c-864e-d04ffd13d006",
+        "https://firebasestorage.googleapis.com/v0/b/qtrivia-4dbfa.appspot.com/o/olympics.mp4?alt=media&token=2ae02f4c-8ddb-464c-9b3e-35f02d550a72",
+        "https://firebasestorage.googleapis.com/v0/b/qtrivia-4dbfa.appspot.com/o/jinyeobkim.mp4?alt=media&token=33dd2bc4-0643-4127-a25c-d3297841340d",
+        "https://firebasestorage.googleapis.com/v0/b/qtrivia-4dbfa.appspot.com/o/lobsterrachel.mp4?alt=media&token=d9034299-efc6-4713-b431-4e05752be1dd",
+        "https://firebasestorage.googleapis.com/v0/b/qtrivia-4dbfa.appspot.com/o/newflat.mp4?alt=media&token=dbb1185a-3dec-4ba6-b01e-5ca60e265661",
+        "https://firebasestorage.googleapis.com/v0/b/qtrivia-4dbfa.appspot.com/o/beer%20question.mp4?alt=media&token=50189438-9697-410a-89fa-f30cc083f912",
+        "https://firebasestorage.googleapis.com/v0/b/qtrivia-4dbfa.appspot.com/o/newalanturing.mp4?alt=media&token=3f159758-0c2a-4f3f-9128-126e2c451cdc",
+        "https://firebasestorage.googleapis.com/v0/b/qtrivia-4dbfa.appspot.com/o/jurassic%20park.mp4?alt=media&token=6e907576-b7fe-416c-82c9-b4b2b5312c4c",
+        "https://firebasestorage.googleapis.com/v0/b/qtrivia-4dbfa.appspot.com/o/jaylanEnding.mp4?alt=media&token=eb2e32a9-753f-4478-b372-de9ab73de4d7"
         
       ]
     }
